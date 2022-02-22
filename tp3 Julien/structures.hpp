c7a6b62ae82a2e2b9d820535c28e9c1cc6e0aa24
Julien9969 <julien.roux404@gmail.com>
2022-02-22 14:37:01 -0500

    lambda

--- tp3 Julien/structures.hpp
+++ tp3 Julien/structures.hpp
@@ -2,90 +2,71 @@
 // Structures mémoires pour une collection de films.
 
 #include <string>
-#include <cassert>
-#include "gsl/span"
-#include <memory>
-using gsl::span;
-using namespace std;
+
 struct Film; struct Acteur; // Permet d'utiliser les types alors qu'ils seront défini après.
 
 class ListeFilms {
+
 public:
 	ListeFilms() = default;
-	ListeFilms(const std::string& nomFichier);
-	ListeFilms(const ListeFilms& l) { assert(l.elements == nullptr); } // Pas demandé dans l'énoncé, mais on veut s'assurer qu'on ne fait jamais de copie de liste, car la copie par défaut ne fait pas ce qu'on veut.  Donc on ne permet pas de copier une liste non vide (la copie de liste vide est utilisée dans la création d'un acteur).
-	~ListeFilms();
-	void ajouterFilm(shared_ptr<Film> film);
-	void enleverFilm(const shared_ptr<Film> film);
-	shared_ptr<Acteur> trouverActeur(const std::string& nomActeur) const;
-	span<shared_ptr<Film>> enSpan() const;
-	int size() const { return nElements; }
 
-	shared_ptr<Film>& operator[] (int index){
-		return this->elements[index];
+	ListeFilms(int capacite, int nElements, Film** elements) : capacite_(capacite), nElements_(nElements), elements_(elements) {}
+
+	void ajoutCapacite (int capacite) {
+		capacite_ = capacite;
+	}
+	void ajout_nElement (int nElements) {
+		nElements_ = nElements;
+	}
+	void ajoutList(Film** elements) {
+		elements_ = elements;
+	}
+
+	const int lireCapacite() const{
+		return capacite_;
+	}
+	const int lire_nElement() const{
+		return nElements_;
+	}
+	Film** lireElements() const{
+		return elements_;
 	}
 	
+	void operator--(int) {
+		nElements_--;
+	}
+
+	void ajoutFilmDansListe(Film* film);
+
+	void enleverFilm(Film* film);
+
+	void detruireListFilms();
+
+	void detruireFilm(int indexFilm);
+
+	Acteur* trouverActeur(std::string nomActeur);
+
+	void afficherListeFilms() const;
 
 private:
-	void changeDimension(int nouvelleCapacite);
-
-	int capacite = 0, nElements = 0;
-	unique_ptr<shared_ptr<Film>[]> elements = nullptr; // Pointeur vers un tableau de Film*, chaque Film* pointant vers un Film.
-	bool possedeLesFilms_ = false; // Les films seront détruits avec la liste si elle les possède.
+	int capacite_, nElements_;
+	Film** elements_; // Pointeur vers un tableau de Film*, chaque Film* pointant vers un Film.
 };
 
-class ListeActeurs {
-public:
-	ListeActeurs() = default;
-	ListeActeurs(int nElements) : nElements_(nElements), elements_(make_unique<shared_ptr<Acteur>[]>(nElements)), capacite_(nElements) {};
-	int lire_nElements() const { return nElements_; };
-	span<shared_ptr<Acteur>> spanListeActeurs() const;
-
-	shared_ptr<Acteur>& operator[] (int index) {
-		return this->elements_[index];
-	}
-
-	
-
-
-	/*
-	ListeActeurs& operator= (ListeActeurs& autre) noexcept {
-		capacite_ = autre.capacite_;
-		nElements_ = autre.nElements_;
-		for (int i : range(nElements_))
-		elements_[i] = autre.elements_[i];
-		return *this;
-	}
-	*/
-private:
-	int capacite_ = 0, nElements_ = 0;
-	unique_ptr<shared_ptr<Acteur>[]> elements_; // Pointeur vers un tableau de Acteur*, chaque Acteur* pointant vers un Acteur.
+struct ListeActeurs {
+	int capacite, nElements;
+	Acteur** elements; // Pointeur vers un tableau de Acteur*, chaque Acteur* pointant vers un Acteur.
 };
 
 struct Film
 {
-	friend ostream& operator<< (ostream& o, const Film& f);
 	std::string titre, realisateur; // Titre et nom du réalisateur (on suppose qu'il n'y a qu'un réalisateur).
-	int anneeSortie = 0, recette = 0; // Année de sortie et recette globale du film en millions de dollars
+	int anneeSortie, recette; // Année de sortie et recette globale du film en millions de dollars
 	ListeActeurs acteurs;
-
-	//Film(const Film& autre) : titre(autre.titre), realisateur(autre.realisateur), anneeSortie(autre.anneeSortie), recette(autre.recette), acteurs(autre.acteurs) {};
-
-	//Film& operator= (Film& autre)  noexcept = default;
-		
-		/*titre = autre.titre;
-		realisateur = autre.realisateur;
-		anneeSortie = autre.anneeSortie;
-		recette = autre.recette;
-		acteurs = autre.acteurs;
-		return *this;*/
-
-
-
 };
 
 struct Acteur
 {
 	std::string nom; int anneeNaissance; char sexe;
-	//ListeFilms joueDans;
+	ListeFilms joueDans;
 };
